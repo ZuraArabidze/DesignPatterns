c02fc4e598531f55eaa4157b3473524915b8ec6b
ZuraArabidze <z.arabidzee@gmail.com>
2025-07-30 09:14:12 +0400

    Strategy Pattern

---
+++ Behavioral/Strategy.cs
@@ -0,0 +1,51 @@
+﻿namespace DesignPatterns.Behavioral;
+
+public class PaymentContext
+{
+    private IPaymentStrategy _paymentStrategy;
+
+    public PaymentContext(IPaymentStrategy paymentStrategy)
+    {
+        _paymentStrategy = paymentStrategy;
+    }
+
+    public void SetStrategy(IPaymentStrategy paymentStrategy)
+    {
+        _paymentStrategy = paymentStrategy;
+    }
+
+    public void ExecutePayment(decimal amount)
+    {
+        _paymentStrategy.Pay(amount);
+    }
+}
+
+
+public interface IPaymentStrategy
+{
+    void Pay(decimal amount);
+}
+
+public class CreditCardPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Paid {amount} using Credit Card.");
+    }
+}
+
+public class PayPalPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Paid {amount} using PayPal.");
+    }
+}
+
+public class BitcoinPayment : IPaymentStrategy
+{
+    public void Pay(decimal amount)
+    {
+        Console.WriteLine($"Paid {amount} using Bitcoin.");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using DesignPatterns.Behavioral;
+
+// Strategy Design Pattern
+
+PaymentContext context = new PaymentContext(new CreditCardPayment());
+context.ExecutePayment(200);
+
+context.SetStrategy(new BitcoinPayment());
+context.ExecutePayment(400);
+
+context.SetStrategy(new PayPalPayment());
+context.ExecutePayment(800);
